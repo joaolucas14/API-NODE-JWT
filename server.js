d144574051a50117942d36c4c0872eca135708ab
joaolucas14 <jlparceiro10@gmail.com>
2025-02-24 09:21:34 -0300

    Adicionando configurações no Prisma

--- server.js
+++ server.js
@@ -7,5 +7,3 @@ app.use(express.json());
 app.use("/", publicRoutes);
 
 app.listen(3000, () => console.log("Servidor rodando na porta 3000"));
-
-// mongodb+srv://joaolucans:<db_password>@cluster0.n6ejc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
