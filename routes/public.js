f03606a0f18f330aa69fee0bbbd6a6be4dde282c
joaolucas14 <jlparceiro10@gmail.com>
2025-02-24 11:11:29 -0300

    Adiciona autenticação e rotas para cadastro e login de usuários

--- routes/public.js
+++ routes/public.js
@@ -1,10 +1,60 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
+const prisma = new PrismaClient();
 const router = express.Router();
 
-router.post("/cadastro", (req, res) => {
-  const user = req.body;
-  res.status(201).json(user);
+const JWT_SECRET = process.env.JWT_SECRET;
+
+router.post("/cadastro", async (req, res) => {
+  try {
+    const user = req.body;
+
+    // encripta a senha
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(user.password, salt);
+
+    //cria o usuario no db
+    const userDb = await prisma.user.create({
+      data: {
+        email: user.email,
+        name: user.name,
+        password: hashPassword,
+      },
+    });
+    res.status(201).json(userDb);
+  } catch {
+    res.status(500).json({ message: "Erro no servidor, tente novamente" });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const userInfo = req.body;
+
+    //verifica se o usuário existe
+    const user = await prisma.user.findUnique({
+      where: { email: userInfo.email },
+    });
+    if (!user) {
+      return res.status(404).json({ message: "Usuário não encontrado" });
+    }
+
+    //verifica se a senha está correta
+    const isMatch = await bcrypt.compare(userInfo.password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({ message: "Senha inválida" });
+    }
+
+    const token = jwt.sign({ id: user.id }, JWT_SECRET, { expiresIn: "1d" });
+
+    res.status(200).json(token);
+  } catch {
+    res.status(500).json({ message: "Erro no servidor, tente novamente" });
+  }
 });
 
 export default router;
